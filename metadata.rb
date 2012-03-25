2d35179cb9e7e5e6007d8900a29cc23d2ff10872
Hasan <martinhbramwell@gmail.com>
2012-03-25 06:16:27 -0500

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "FLEETING CLOUDS"
+maintainer_email "<martinhbramwell (0) gmail o com>"
+license          "All rights reserved"
+description      "Installs psiprobe"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
